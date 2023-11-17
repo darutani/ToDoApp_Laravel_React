8989f7ffb1084d2952b8544687e9fe4608e9876f
natsuki <su811sh1@gmail.com>
2023-11-17 23:00:23 +0900

    🌐 日本語対応のためaskdkc/breezejpをインストール

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -43,6 +43,36 @@ class TaskTest extends TestCase
             ->assertJsonFragment($data);
     }
 
+    public function test_タイトルが空の場合は実行できない(): void
+    {
+        $data = [
+            'title' => ''
+        ];
+        $response = $this->postJson('api/tasks', $data);
+        // dd($response->json());
+
+        $response
+            ->assertStatus(422)
+            ->assertJsonValidationErrors([
+                'title' => 'タイトルは必ず指定してください。'
+            ]);
+    }
+
+    public function test_タイトルが256文字以上の場合には登録できない(): void
+    {
+        $data = [
+            'title' => str_repeat('あ', 256)
+        ];
+        $response = $this->postJson('api/tasks', $data);
+        // dd($response->json());
+
+        $response
+            ->assertStatus(422)
+            ->assertJsonValidationErrors([
+                'title' => 'タイトルは、255文字以下で指定してください。'
+            ]);
+    }
+
     /**
      * updateアクションに接続した際にTaskデータが更新されること
      *
