491e5021b6a3f91850aa8a3e508143c76cfb9f7f
NatsukiHayashi <su811sh1@gmail.com>
2023-12-11 23:53:54 +0900

    ✨ vue-queryを導入

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -1,12 +1,23 @@
 import { createApp } from 'vue';
-import{ createVuetify } from 'vuetify'
-import App from "./vue/App.vue"
-import 'vuetify/dist/vuetify.min.css'
-import router from './router'
+import{ createVuetify } from 'vuetify';
+import App from "./vue/App.vue";
+import 'vuetify/dist/vuetify.min.css';
+import router from './router';
+import { QueryClient, VUE_QUERY_CLIENT } from 'vue-query';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+          // リトライの回数を設定
+          retry: false, // デフォルトは3回
+        },
+    },
+});
 
 const app = createApp(App);
 const vuetify = createVuetify();
 
 app.use(vuetify)
 app.use(router)
+app.provide(VUE_QUERY_CLIENT, queryClient);
 app.mount('#app');
