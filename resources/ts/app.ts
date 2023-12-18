a42ca3fd0c29c9d3c300923c5b89202b46baf23d
NatsukiHayashi <su811sh1@gmail.com>
2023-12-18 23:34:26 +0900

    ➕ vue3-toastifyの設定を追加

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -4,6 +4,7 @@ import App from "./vue/App.vue";
 import 'vuetify/dist/vuetify.min.css';
 import router from './router';
 import { QueryClient, VUE_QUERY_CLIENT } from 'vue-query';
+import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify';
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -21,3 +22,7 @@ app.use(vuetify)
 app.use(router)
 app.provide(VUE_QUERY_CLIENT, queryClient);
 app.mount('#app');
+app.use(Vue3Toastify, {
+    autoClose: 3000,
+    hideProgressBar: true,
+  } as ToastContainerOptions);
