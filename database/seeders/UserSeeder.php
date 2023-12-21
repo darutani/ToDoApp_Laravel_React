365af0c539a97d7ba22ee1569b9eeefef328fbad
NatsukiHayashi <su811sh1@gmail.com>
2023-12-21 09:19:53 +0900

    🌱 Userのダミーデータを作成

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \DB::table('users')->insert([
+            [
+                'name' => 'admin',
+                'email' => 'admin@examplec.com',
+                'email_verified_at' => now(),
+                'password' => \Hash::make('123456789'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'yamada',
+                'email' => 'yamada@examplec.com',
+                'email_verified_at' => now(),
+                'password' => \Hash::make('123456789'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'tanaka',
+                'email' => 'tanaka@examplec.com',
+                'email_verified_at' => now(),
+                'password' => \Hash::make('123456789'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
