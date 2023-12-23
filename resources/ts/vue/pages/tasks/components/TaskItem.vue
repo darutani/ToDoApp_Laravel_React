ad7b9775adeeb9b39549b3705d548b469a2d9aae
NatsukiHayashi <su811sh1@gmail.com>
2023-12-23 18:08:37 +0900

    ✨ タスクタイトルの更新・削除の実装

--- resources/ts/vue/pages/tasks/components/TaskItem.vue
+++ resources/ts/vue/pages/tasks/components/TaskItem.vue
@@ -1,24 +1,68 @@
 <template>
     <li :class="{ 'done': task.is_done }">
         <label class="checkbox-label">
-            <input type="checkbox" class="checkbox-input" @click="handleDoneChange(task)" />
+            <input type="checkbox" class="checkbox-input" @click="handleDoneChange()" />
         </label>
-        <div><span>{{ task.title }}</span></div>
-        <button class="btn is-delete">削除</button>
+        <div v-if=" !isEditing" @click="handleToggleEdit()">
+            <span>{{ task.title }}</span>
+        </div>
+        <form v-else @submit.prevent="handleUpdate">
+            <input type="text" class="input" v-model="editTitle" @keydown="handleOnKey">
+        </form>
+        <button class="btn is-delete" v-if="!isEditing" @click="handleDelete">削除</button>
+        <button class="btn" v-else @click="handleUpdate">更新</button>
     </li>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import { Task } from "@/types/Task";
 import { useUpdateDoneTask, useUpdateTask, useDeleteTask } from "@/queries/TaskQuery";
+import { toast } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
 
 const props = defineProps<{
     task: Task;
 }>()
 
 const updateDoneTask = useUpdateDoneTask();
+const updateTask = useUpdateTask();
+const deleteTask = useDeleteTask();
 
-const handleDoneChange = (task: Task) => {
-    updateDoneTask.mutate(task)
+const isEditing = ref(false);
+const editTitle = ref("");
+
+const handleDoneChange = () => {
+    updateDoneTask.mutate(props.task)
+}
+
+const handleToggleEdit = () => {
+    isEditing.value = true;
+    editTitle.value = props.task.title;
+}
+
+const handleOnKey = (e: KeyboardEvent) => {
+    if (['Escape', 'Tab'].includes(e.key)) {
+        isEditing.value = false;
+    }
+}
+
+const handleUpdate = () => {
+    if (!editTitle.value) {
+        toast.error('タイトルを入力してください')
+        return
+    }
+
+    const newTask = { ...props.task, title: editTitle.value }
+
+    updateTask.mutate({
+        id: props.task.id,
+        task: newTask,
+    });
+    isEditing.value = false;
+}
+
+const handleDelete = () => {
+    deleteTask.mutate(props.task.id);
 }
 </script>
