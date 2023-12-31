efd89a08c62480b4348ca77c0a9da7f2b591c04e
NatsukiHayashi <su811sh1@gmail.com>
2023-12-31 11:56:26 +0900

    🔥 不要なテンプレ用タスクを削除

--- resources/ts/vue/pages/tasks/components/TaskList.vue
+++ resources/ts/vue/pages/tasks/components/TaskList.vue
@@ -2,41 +2,6 @@
     <div class="inner">
         <ul class="task-list">
             <TaskItem v-for="task in tasks" :key="task.id" :task="task" />
-            <li>
-                <label class="checkbox-label">
-                    <input type="checkbox" class="checkbox-input" />
-                </label>
-                <div><span>新しいTODO</span></div>
-                <button class="btn is-delete">削除</button>
-            </li>
-            <li>
-                <label class="checkbox-label">
-                    <input type="checkbox" class="checkbox-input" />
-                </label>
-                <form><input type="text" class="input" value="編集中のTODO" /></form>
-                <button class="btn">更新</button>
-            </li>
-            <li class="done">
-                <label class="checkbox-label">
-                    <input type="checkbox" class="checkbox-input" />
-                </label>
-                <div><span>実行したTODO</span></div>
-                <button class="btn is-delete">削除</button>
-            </li>
-            <li>
-                <label class="checkbox-label">
-                    <input type="checkbox" class="checkbox-input" />
-                </label>
-                <div><span>ゴミ捨て</span></div>
-                <button class="btn is-delete">削除</button>
-            </li>
-            <li>
-                <label class="checkbox-label">
-                    <input type="checkbox" class="checkbox-input" />
-                </label>
-                <div><span>掃除</span></div>
-                <button class="btn is-delete">削除</button>
-            </li>
         </ul>
     </div>
 </template>
