0a3a8288d900b8bd2966529f103ad5f140c51a0b
NatsukiHayashi <su811sh1@gmail.com>
2023-12-31 11:18:42 +0900

    ✨ 指定URL以外へ遷移した時のエラーページを生成

---
+++ resources/ts/vue/pages/error/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="align-center">
+        <h1>404 NotFound</h1>
+        <p>お探しのページは見つかりませんでした。</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+
+</style>
